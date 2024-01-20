07c0f63c596f417a56df6f92d811c089627b8986
Okan Okta <oktaokan56@gmail.com>
2024-01-20 16:04:01 +0300

    the service, to, configuration and controller layers were created

--- src/main/java/com/software/hotelReservation/repository/HotelRepository.java
+++ src/main/java/com/software/hotelReservation/repository/HotelRepository.java
@@ -1,9 +1,10 @@
 package com.software.hotelReservation.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 import com.software.hotelReservation.model.Hotel;
 
+@Repository
 public interface HotelRepository extends JpaRepository<Hotel, Long>{
 
 }
