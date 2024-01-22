02fd019c4157c69387e458d33714d9fdb194701e
Okan Okta <oktaokan56@gmail.com>
2024-01-22 15:59:39 +0300

    a class called room was created and associated with the hotel

--- src/main/java/com/software/hotelReservation/model/Hotel.java
+++ src/main/java/com/software/hotelReservation/model/Hotel.java
@@ -1,10 +1,13 @@
 package com.software.hotelReservation.model;
 
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -21,4 +24,7 @@ public class Hotel {
 	
 	@Column(name="address")
 	private String address;
+	
+	@OneToMany(mappedBy = "hotel")
+	private List<Room> rooms;
 }
---
+++ src/main/java/com/software/hotelReservation/model/Room.java
@@ -0,0 +1,35 @@
+package com.software.hotelReservation.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="room")
+public class Room {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="room_id")
+	private Long roomId;
+	
+	@Column(name="room_number")
+	private int roomNumber;
+	
+	@Column(name="room_type")
+	private String roomType;
+	
+	@Column(name="price")
+	private double price;
+	
+	@ManyToOne()
+	@JoinColumn(name="hotel_id")
+	private Hotel hotel;
+
+}
